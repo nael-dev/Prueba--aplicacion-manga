b24070a63467aca0669140fe4ff35312e2427877
Nael <naeldiaz@proton.me>
2025-07-17 16:24:21 +0000

    tablas definidas

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,72 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column,relationship
+from sqlalchemy import DateTime
+from datetime import datetime
+from typing import List
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
+    name: Mapped[str] = mapped_column(nullable=False)
+    last_name: Mapped[str] = mapped_column(nullable=False)
+    image: Mapped[str] = mapped_column(String(200), nullable=False)
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "name": self.name,
+            "last_name": self.last_name
             # do not serialize the password, its a security breach
         }
+
+
+class Libro(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(120), nullable=False)
+    author: Mapped[str] = mapped_column(String(120), nullable=False)
+    pages: Mapped[int] = mapped_column(nullable=False)
+    editorial: Mapped[str] = mapped_column(String(120), nullable=False)
+    isbn: Mapped[int] = mapped_column(nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "author": self.author,
+            "pages": self.pages,
+            "editorial": self.editorial,
+            "isbn": self.isbn
+        }
+
+
+class Editorial(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name
+        }
+
+
+class Author(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name
+        }
+    
+class MyComics(db.Model):
+    id:Mapped[int]= mapped_column(primary_key= True)
+    libro_id: Mapped[int] = mapped_column(ForeignKey("libro.id")) # type: ignore
